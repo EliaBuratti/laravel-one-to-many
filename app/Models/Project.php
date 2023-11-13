19aafa92e10889d580841d0faf02ebb177acd0b5
EliaBuratti <elia.buratti.dev@gmail.com>
2023-11-13 16:23:28 +0100

    add migation, seed and connect to projects table

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
@@ -16,7 +17,13 @@ class Project extends Model
         return Str::slug($title, '-');
     }
 
-    protected $fillable = ['title', 'description', 'slug', 'cover_image', 'skills', 'project_link'];
+    protected $fillable = ['title', 'description', 'slug', 'cover_image', 'skills', 'project_link', 'type_id'];
+
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 
     /*     protected function cover_image(): Attribute
     {
